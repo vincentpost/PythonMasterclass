75f25f04c927c60850798d7331fc3dc06e74f894
Cristina Solorzano <cristina.solorzano@flinders.edu.au>
2023-04-28 22:58:24 +0930

    Myhomework

--- first_exercise.py
+++ first_exercise.py
@@ -11,11 +11,13 @@ to the conditionals list below and adding a message to be printed to the
 screen. 
 """
 
-name = "YourName"
+name = "Cristina"
 
 if name == "Vincent":
     print("Vincent is one of the instructors of the Python Masterclass")
 elif name == "Anushree":
     print("Anushree is the AWS support person during the Python Masterclass")
+elif name == "Cristina":
+    print("Cristina helps with the Q&A chat")
 else:
     print("Name is unknown, not sure what to print about this person.")
