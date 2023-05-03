94b3a54f5c077029b357388d3d646d8331f80b73
vincentpost <info@edinsi.nl>
2023-05-03 09:03:33 +0200

    final test

--- first_exercise.py
+++ first_exercise.py
@@ -18,4 +18,4 @@ if name == "Vincent":
 elif name == "Anushree":
     print("Anushree is the AWS support person during the Python Masterclass")
 else:
-    print("Name is unknown, not sure what to print about this person.")
+    print("Name is unknown, not sure what message to print for this person.")
