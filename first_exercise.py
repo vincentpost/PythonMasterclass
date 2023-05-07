11b3f6c9c5fbcfc9e78b5cc6a57ec166f11f2632
Richard White <richard.white@des.qld.gov.au>
2023-05-08 08:31:19 +1000

    Add name to  conditionals

--- first_exercise.py
+++ first_exercise.py
@@ -11,11 +11,13 @@ to the conditionals list below and adding a message to be printed to the
 screen. 
 """
 
-name = "YourName"
+name = "Richard"
 
 if name == "Vincent":
     print("Vincent is one of the instructors of the Python Masterclass")
 elif name == "Anushree":
     print("Anushree is the AWS support person during the Python Masterclass")
+elif name == "Richard":
+    print("Richard is a Hydrologist with DES")
 else:
     print("Name is unknown, not sure what to print about this person.")
