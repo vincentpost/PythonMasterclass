be0b9fb718bd35db63392f94eeb68aa014658f3f
gberthaly-martyn <107163842+gberthaly-martyn@users.noreply.github.com>
2023-05-08 12:11:59 +1000

    Added name "Georgianna" to the conditionals list.

--- first_exercise.py
+++ first_exercise.py
@@ -17,5 +17,7 @@ if name == "Vincent":
     print("Vincent is one of the instructors of the Python Masterclass")
 elif name == "Anushree":
     print("Anushree is the AWS support person during the Python Masterclass")
+elif name == "Georgianna":
+    print("Georgianna is a participant in the Python Masterclass")
 else:
     print("Name is unknown, not sure what to print about this person.")
