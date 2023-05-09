c9cf933a696ab02cc08961aeff0104e7f97a43b2
changhaojin <changhao.jin@mdba.gov.au>
2023-05-09 16:18:57 +1000

    Add my name.

--- first_exercise.py
+++ first_exercise.py
@@ -11,11 +11,13 @@ to the conditionals list below and adding a message to be printed to the
 screen. 
 """
 
-name = "YourName"
+name = "Changhao"
 
 if name == "Vincent":
     print("Vincent is one of the instructors of the Python Masterclass")
 elif name == "Anushree":
     print("Anushree is the AWS support person during the Python Masterclass")
+elif name == "Changhao":
+    print("Changhao is one of the participants of the Python Masterclass.")    
 else:
     print("Name is unknown, not sure what to print about this person.")
