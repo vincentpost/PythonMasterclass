910263d2dc29ebf1b61b55257243906e46173e87
qkdnf414 <paul.sim@des.qld.gov.au>
2023-05-11 14:51:48 +1000

    Homework with name Paul

--- first_exercise.py
+++ first_exercise.py
@@ -11,11 +11,13 @@ to the conditionals list below and adding a message to be printed to the
 screen. 
 """
 
-name = "YourName"
+name = "Paul"
 
 if name == "Vincent":
     print("Vincent is one of the instructors of the Python Masterclass")
 elif name == "Anushree":
     print("Anushree is the AWS support person during the Python Masterclass")
+elif name == "Paul":
+    print("Paul is the AWS student in the Python Masterclass")
 else:
     print("Name is unknown, not sure what to print about this person.")
