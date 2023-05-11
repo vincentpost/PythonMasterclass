1e63099250e291da5cba2f25e19726c7287d5ae1
khilrajpandeya <khilraj.pandeya@des.qld.gov.au>
2023-05-11 12:21:27 +1000

    My homework - Khil

--- first_exercise.py
+++ first_exercise.py
@@ -11,11 +11,13 @@ to the conditionals list below and adding a message to be printed to the
 screen. 
 """
 
-name = "YourName"
+name = "Khil"
 
 if name == "Vincent":
     print("Vincent is one of the instructors of the Python Masterclass")
 elif name == "Anushree":
     print("Anushree is the AWS support person during the Python Masterclass")
+elif name == "Khil":
+    print("Khil is the attendee of the Python Masterclass")
 else:
     print("Name is unknown, not sure what to print about this person.")
