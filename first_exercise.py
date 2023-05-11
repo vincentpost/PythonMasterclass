2cd0aded0530155174c8cb2ca09f0b518a4fadfe
borisfichot <borisfichot@gmail.com>
2023-05-10 21:44:34 -0700

    Boris Additions

--- first_exercise.py
+++ first_exercise.py
@@ -11,11 +11,13 @@ to the conditionals list below and adding a message to be printed to the
 screen. 
 """
 
-name = "YourName"
+name = "Boris"
 
 if name == "Vincent":
     print("Vincent is one of the instructors of the Python Masterclass")
 elif name == "Anushree":
     print("Anushree is the AWS support person during the Python Masterclass")
+elif name == "Boris":
+    print("Boris has no clue how to use Python") 
 else:
     print("Name is unknown, not sure what to print about this person.")
