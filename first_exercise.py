abfa0df94bfd9ebc3a873654efbdfb9d719b71f5
Konrad Miotlinski <konrad.miotlinski@gmail.com>
2023-05-16 17:32:05 +0800

    Modified from VSC

--- first_exercise.py
+++ first_exercise.py
@@ -11,11 +11,15 @@ to the conditionals list below and adding a message to be printed to the
 screen. 
 """
 
-name = "YourName" # I could edit on the website
+name = "Konrad" # I could edit on the website
 
 if name == "Vincent":
     print("Vincent is one of the instructors of the Python Masterclass")
 elif name == "Anushree":
     print("Anushree is the AWS support person during the Python Masterclass")
+elif name == "Konrad":
+    print("Konrad is the participant of the Python Masterclass")
 else:
     print("Name is unknown, not sure what to print about this person.")
+
+print("This is the end of the program!")
