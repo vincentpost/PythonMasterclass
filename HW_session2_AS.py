26d688e5b2b794a923320e3180d43d7ac040dd4e
a-stoddart <andriana.stoddart@dcceew.gov.au>
2023-05-31 11:41:04 +1000

    modified:   HW_session2_AS.py

--- HW_session2_AS.py
+++ HW_session2_AS.py
@@ -10,25 +10,25 @@ class Well:
     def __str__(self):
         return f"Borefield: {self.bf}, coordinates: {self.xcoord, self.ycoord}, extractrion rate: {self.extr_rate}, casing radius: {self.rad}"
 
-#wellx = Well("Surat",145,-33,20,300)
+wellx = Well("Surat",145,-33,20,300)
 print(Well("Surat",145,-33,20,300))
 
 
-# class Wellfield:
-#     def __init__(self):
-#         self._wellfield = []
+class Wellfield:
+    def __init__(self):
+        self._wellfield = []
     
-#     def add_well(self, well, borefield="Surat"):
-#         if borefield == "Surat":
-#             self._wellfield.append(well)
+    def add_well(self, well, borefield="Surat"):
+        if borefield == "Surat":
+            self._wellfield.append(well)
     
-#     def list_wells(self):
-#         print("Surat borefield")
-#         for well in self._wellfield:
-#             print('\t', well)
+    def list_wells(self):
+        print("Surat borefield")
+        for well in self._wellfield:
+            print('\t', well)
         
-# w = Wellfield()
-# # wellx = Well("Surat",145,-33,20,300)
+w = Wellfield()
+# wellx = Well("Surat",145,-33,20,300)
 
-# w.add_well(wellx)#I thought I had done this like the example, but apparently add_well is missing the positional argument "well". Where have I gone wrong?
-# w.list_wells()
+w.add_well(wellx)
+w.list_wells()
